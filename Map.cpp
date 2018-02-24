b28c5c4189bf07ed6750ab304b5b59dc15911538
mgotao <m.gotao@yahoo.com>
2018-02-24 10:38:42 -0800

    Add 2D array functionality.

--- Map.cpp
+++ Map.cpp
@@ -14,17 +14,27 @@ Map::~Map(){
 }
 
 void Map::initMap(string file, int& dimensions, Map& map){
+	//read file and get dimension of map
 	char text;
 	ifstream fileIn;
 	fileIn.open(file.c_str());
 	fileIn >> dimensions;
+
+	//initialize 2D array to simulate map
+	char** m_Map = new char*[dimensions];
+	for(int i = 0; i < dimensions; ++i)
+	{
+		m_Map[i] = new char[dimensions];
+	}
+
+	//read map from file and assign to 2D array
 	for(int row = 0; row < dimensions; ++row)
 	{
 		for(int column = 0; column < dimensions; ++column)
 		{
 			fileIn >> text;
-			cout << text;
-			//map[row][column] = text;
+			m_Map[row][column] = text;
+			cout << m_Map[row][column];
 		}
 		cout << '\n';
 	}
