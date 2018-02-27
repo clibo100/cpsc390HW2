4740fc2a93300c9cbd134559a88e7296d4d1698d
mgotao <m.gotao@yahoo.com>
2018-02-27 12:45:02 -0800

    Add gitignore

--- Robot.cpp
+++ Robot.cpp
@@ -36,12 +36,5 @@ void Robot:: initRobot(char**& map, int& dimensions)
 
 void Robot:: traverseMap(char**& map, int**& distMap, int& dimensions)
 {
-	/*
-	i hope this is where all the good shit happens. 
-	like, what i'm thinking is that this will be the function that you call
-	when you have the initial maps and you just straight up want the 
-	final path. so like all of the calculation functions should also
-	be in this class and implemented by this function if
-	that makes sense?
-	*/
+
 }
